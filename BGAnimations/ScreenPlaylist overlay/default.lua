a7b4bffce91699c2091436477b4aaf64fb4ec4f7
Anthony Fernandez <tchalogen@gmail.com>
2023-07-22 18:23:45 -0400

    Added

--- BGAnimations/ScreenPlaylist overlay/default.lua
+++ BGAnimations/ScreenPlaylist overlay/default.lua
@@ -2,93 +2,136 @@
 local af = Def.ActorFrame {}
 
 -- TODO: leverage Styles.lua to abstract design from logic/drawing
-local boxW = _screen.w/3 - _screen.w/16
-local boxH = _screen.h * 0.75
-local boxColor = {0,0,0,0.3}
-local boxBorderWidth = 2
-local boxBorderColor = {0.5,0.5,0.5,1}
+local boundW = _screen.w/3 - _screen.w/16
+local boundH = _screen.h * 0.75
+local boundColor = {0,0,0,0.5}
+local boundBorderWidth = 2
+local boundBorderColor = {0.5,0.5,0.5,1}
+
+local innerW
+local innerH
+local innerColor
 
 -- Left side box.
 af[#af+1] = Def.ActorFrame {
-    InitCommand=function(self) 
-        self:xy(_screen.cx / 3, _screen.cy)
-        self:zoomto(boxW, boxH)
-    end,
+  InitCommand=function(self) 
+    self:xy(_screen.cx / 3, _screen.cy)
+    self:zoomto(boundW, boundH)
+  end,
 
-    Def.Quad {
-        InitCommand=function(self)
-            self:xy(0, 0)
-            self:diffuse(0.5, 0.5, 0.5, 0.25)
-        end
-    },
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:diffuse(boundColor)
+    end
+  },
 
-    Def.Quad {
-        InitCommand=function(self)
-            self:xy(0, 0)
-            self:zoomto(1, 0.2)
-            self:valign(0)
-            self:diffuse(0, 0.5, 1, 1)
-        end
-    },
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:zoomto(1, 0.2)
+      self:valign(0)
+      self:diffuse(0, 0.5, 1, 1)
+    end
+  },
 
-    Def.Quad {
-        InitCommand=function(self)
-            self:xy(0, 0)
-            self:zoomto(1, 0.2)
-            self:valign(1)
-            self:diffuse(0.5, 0, 1, 1)
-        end
-    }
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:zoomto(1, 0.2)
+      self:valign(1)
+      self:diffuse(0.5, 0, 1, 1)
+    end
+  }
 }
 
 -- Is this how this has to be done?
 -- I guess it makes sense since it is outlining a bounding box, but still...
-af[#af+1] = Border(boxW, boxH, 2)..{
-    InitCommand=function(self)
-        self:xy(_screen.cx / 3, _screen.cy)
-        self:diffuse(boxBorderColor)
-    end
+af[#af+1] = Border(boundW, boundH, 2)..{
+  InitCommand=function(self)
+      self:xy(_screen.cx / 3, _screen.cy)
+      self:diffuse(boundBorderColor)
+  end
 }
 
+-- Middle box.
+af[#af+1] = Def.ActorFrame {
+  InitCommand=function(self)
+    self:xy(_screen.cx, _screen.cy)
+    self:zoomto(boundW, boundH)
+  end,
 
--- af[#af+1] = Def.Quad {   
---     InitCommand=function(self)
---         self:xy(_screen.cx / 3, _screen.cy);
---         self:zoomto(boxW, boxH):diffuse(boxColor)
---     end
--- }
--- af[#af+1] = Border(boxW, boxH, 2)..{
---     InitCommand=function(self)
---         self:xy(_screen.cx / 3, _screen.cy)
---         self:diffuse(boxBorderColor)
---     end
--- }
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:diffuse(boundColor)
+    end
+  },
 
--- Middle box/border.
--- af[#af+1] = Def.Quad {
---     InitCommand=function(self)
---         self:xy(_screen.cx, _screen.cy)
---         self:zoomto(boxW, boxH):diffuse(boxColor)
---     end
--- }
--- af[#af+1] = Border(boxW, boxH, 2)..{
---     InitCommand=function(self)
---         self:xy(_screen.cx, _screen.cy)
---         self:diffuse(boxBorderColor)
---     end
--- }
+  Def.Quad {
+    InitCommand=function(self)
+        self:xy(0, 0)
+        self:zoomto(1, 0.2)
+        self:valign(0)
+        self:diffuse(0, 0.5, 1, 1)
+    end
+  },
 
--- Right box/border.
--- af[#af+1] = Def.Quad {
---     InitCommand=function(self)
---         self:xy(_screen.cx + _screen.cx/1.5, _screen.cy)
---         self:zoomto(boxW, boxH):diffuse(boxColor)
---     end
--- }
--- af[#af+1] = Border(boxW, boxH, 2)..{
---     InitCommand=function(self)
---         self:xy(_screen.cx + _screen.cx/1.5, _screen.cy)
---         self:diffuse(boxBorderColor)
---     end
--- }
+  Def.Quad {
+      InitCommand=function(self)
+          self:xy(0, 0)
+          self:zoomto(1, 0.2)
+          self:valign(1)
+          self:diffuse(0.5, 0, 1, 1)
+      end
+  },
+}
+
+af[#af+1] = Border(boundW, boundH, 2)..{
+  InitCommand=function(self)
+    self:xy(_screen.cx, _screen.cy)
+    self:diffuse(boundBorderColor)
+  end
+}
+
+-- Right box.
+af[#af+1] = Def.ActorFrame {
+  InitCommand=function(self)
+    self:xy(_screen.cx + (_screen.cx / 1.5), _screen.cy)
+    self:zoomto(boundW, boundH)
+  end,
+
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:diffuse(boundColor)
+    end
+  },
+
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:zoomto(1, 0.2)
+      self:valign(0)
+      self:diffuse(0, 0.5, 1, 1)
+    end
+  },
+
+  Def.Quad {
+    InitCommand=function(self)
+      self:xy(0, 0)
+      self:zoomto(1, 0.2)
+      self:valign(1)
+      self:diffuse(0.5, 0, 1, 1)
+    end
+  }
+}
+
+af[#af+1] = Border(boundW, boundH, 2)..{
+  InitCommand=function(self)
+    self:xy(_screen.cx + (_screen.cx / 1.5), _screen.cy)
+    self:diffuse(boundBorderColor)
+  end
+}
+
 return af
