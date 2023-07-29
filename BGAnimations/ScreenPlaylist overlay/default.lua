74cc1e0a11d46609977b8e565e598265459ec00b
Anthony Fernandez <tchalogen@gmail.com>
2023-07-28 21:22:53 -0400

    Refactor for initial implementation of actor scrolling

--- BGAnimations/ScreenPlaylist overlay/default.lua
+++ BGAnimations/ScreenPlaylist overlay/default.lua
@@ -3,30 +3,34 @@ local Styles = LoadActor("./Styles.lua")
 local af = Def.ActorFrame {}
 
 -- Left side box.
-af[#af+1] = LoadActor("./intro/left.lua", Styles)
-af[#af+1] = Border(Styles.boundW, Styles.boundH, 2)..{
-  InitCommand=function(self)
-    self:xy(_screen.cx / 3, _screen.cy)
-    self:diffuse(Styles.boundBorderColor)
-  end
+af[#af+1] = Def.ActorFrame {
+  LoadActor("./intro/left.lua", Styles),
+  Border(Styles.boundW, Styles.boundH, 2)..{
+    InitCommand=function(self)
+      self:xy(_screen.cx / 3, _screen.cy)
+      self:diffuse(Styles.boundBorderColor)
+    end
+  }
 }
 
--- Middle box.
-af[#af+1] = LoadActor("./intro/middle.lua", Styles)
-af[#af+1] = Border(Styles.boundW, Styles.boundH, 2)..{
-  InitCommand=function(self)
-    self:xy(_screen.cx, _screen.cy)
-    self:diffuse(Styles.boundBorderColor)
-  end
+af[#af+1] = Def.ActorFrame {
+  LoadActor("./intro/middle.lua", Styles),
+  Border(Styles.boundW, Styles.boundH, 2)..{
+    InitCommand=function(self)
+      self:xy(_screen.cx, _screen.cy)
+      self:diffuse(Styles.boundBorderColor)
+    end
+  }
 }
 
--- Right box.
-af[#af+1] = LoadActor("./intro/right.lua", Styles)
-af[#af+1] = Border(Styles.boundW, Styles.boundH, 2)..{
-  InitCommand=function(self)
-    self:xy(_screen.cx + (_screen.cx / 1.5), _screen.cy)
-    self:diffuse(Styles.boundBorderColor)
-  end
+af[#af+1] = Def.ActorFrame {
+  LoadActor("./intro/right.lua", Styles),
+  Border(Styles.boundW, Styles.boundH, 2)..{
+    InitCommand=function(self)
+      self:xy(_screen.cx + (_screen.cx / 1.5), _screen.cy)
+      self:diffuse(Styles.boundBorderColor)
+    end
+  }
 }
 
 return af
