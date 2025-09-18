524b7f1bb05be00f8debd312cbd037d378e18b72
Anthony Fernandez <tchalogen@gmail.com>
2025-09-17 20:16:52 -0400

    Start of new thing maybe...

--- Scripts/99 SL-ThemePrefs.lua
+++ Scripts/99 SL-ThemePrefs.lua
@@ -347,6 +347,12 @@ SL_CustomPrefs.Get = function()
 			Choices =  { THEME:GetString("ThemePrefs","Yes"), THEME:GetString("ThemePrefs", "No") },
 			Values  = { true, false }
 		},
+
+    EnableTest = {
+      Default = false,
+      Choices = { THEME:GetString("ThemePrefs", "Yes"), THEME:GetString("ThemePrefs", "No") },
+      Values = { true, false }
+    },
 		
 		CloseFolderCodes = {
 			Default = "None",
